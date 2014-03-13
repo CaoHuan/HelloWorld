03dfc3aa4a51f44de4fe1ce6537f833921ec5703
CaoHuan <cdh321@sina.cn>
2014-03-13 14:16:49 +0800

    lua

--- lua-5.2.3/src/Source.cpp
+++ lua-5.2.3/src/Source.cpp
@@ -49,32 +49,72 @@ void TestLua()
 
 	}
 
-	int a = 0;
-	int b = 0;
+	// 将表放入栈顶
+	lua_getglobal(L, "table");
 
-	// 获取a的值
-	lua_getglobal(L, "a");
-	if (!lua_isnumber(L, -1))
+	// 将表的"a"字段放入栈顶
+	lua_pushstring(L, "a");
+
+	// 此时表的index为 -2， -1为"a"
+	lua_gettable(L, -2);
+	 
+	if (lua_isnumber(L, -1) )
 	{
-		cout << "-2 error" << endl;
-		return ;
+		cout << "table.a = " << lua_tonumber(L, -1) << endl;
 	}
 
-	a = lua_tonumber(L, -1);
+// 	// 函数名首先压入栈
+// 	lua_getglobal(L, "FunOneArgWithTwoResults");
+// 	
+// 	// 压入参数
+// 	lua_pushstring(L, "message from C++");
+// 
+// 	// 先关注参数， 此时返回值我们指定为0， 参数个数指定为1
+// 	if (lua_pcall(L, 1, 0, 0))
+// 	{
+// 		const char * error = lua_tostring(L, -1) ;
+// 		cout << string(error) << endl;
+// 		return;
+// 	}
 
-	// 获取b的值
-	lua_getglobal(L, "b");
-	if (!lua_isnumber(L, -1))
-	{
-		cout << "-1 error" << endl;
-		return ;
-	}
-	
-	b = lua_tonumber(L, -1);
+// 	// 函数名首先压入栈
+// 	lua_getglobal(L, "FunOneArgWithTwoResults");
+// 
+// 	// 压入参数
+// 	lua_pushstring(L, "message from C++");
+// 
+// 	// 此时返回值我们指定为2
+// 	if (lua_pcall(L, 1, 1, 0))
+// 	{
+// 		const char * error = lua_tostring(L, -1) ;
+// 		cout << string(error) << endl;
+// 		return;
+// 	}
+// 
+// 	if (lua_isstring(L, -1) || lua_isstring(L, -2) )
+// 	{
+// 		// 注意这里的顺序，第一个返回值首先入栈，所以栈顶是第二个返回值
+// 		// -2才是第一个返回值的索引
+// 		const char *firstResult = lua_tostring(L, -1);
+// 		//const char *secondResult = lua_tostring(L, -1);
+// 
+// 		// 在这里还要注意，返回的两个指针如果需要在其他的地方使用，
+// 		// 要将这两个指针的内容copy下来一份，然后指向copy的那个内存
+// 		// 不然lua回收栈中的数据后
+// 		// 这两个指针就失效了
+// 		cout << "第一个返回值: " << firstResult << endl;
+// 		//cout << "第二个返回值: " << secondResult << endl;
+// 	}
+// 	
 
-	cout << "a = " << a << " b = " << b << endl;
-	cout << "a + b = " << a + b << endl;
+
+// 	cout << endl;
+// 	
+// 	lua_getglobal(L, "FunOneArg");
+// 	lua_pushstring(L, "text from C++");
+// 	lua_pcall(L, 1, 0, 0);
+
 
 	lua_close(L);
 
-}
+} 
